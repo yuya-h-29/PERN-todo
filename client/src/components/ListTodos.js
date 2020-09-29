f220748b3a847c75e78538f72802e3a5416fbe73
yuya-h-29 <tzou11291996@gmail.com>
2020-09-29 11:54:05 +0900

    add delete button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,20 @@ import React, { Fragment, useEffect, useState } from "react";
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  // delete function
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
@@ -32,10 +46,17 @@ const ListTodos = () => {
         </thead>
         <tbody>
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
